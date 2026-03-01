a232db5034a483fca47725b65ade31cab5b77afd
Rayyan Khan <iamhimandhimalso@gmail.com>
2026-03-02 02:23:08 +0530

    Updated OOPSBanner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -2,12 +2,12 @@
 public class OOPSBanner {
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****   *****    ******");
-        System.out.println("*     *  *     *  *     *  *     ");
-        System.out.println("*     *  *     *  *     *  *     ");
-        System.out.println("*     *  *     *  *****    ******");
-        System.out.println("*     *  *     *  *             *");
-        System.out.println("*     *  *     *  *             *");
-        System.out.println(" *****    *****   *        ******");
+        System.out.println(" *****    *****   *****    ****** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *****    ****** ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println(" *****    *****   *        ****** ");
     }
 }
